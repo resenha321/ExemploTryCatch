7d1231e2faeaa577ef8627736b0749e05aa93ffa
Resenha321 <Fernandozribeiro.fr@gmail.com>
2022-03-15 17:18:42 -0300

    Adicionando GitHelloApp com a main() - v1.0

--- src/main/java/com/mycompany/exemplotrycatch/ExmploTry.java
+++ src/main/java/com/mycompany/exemplotrycatch/ExmploTry.java
@@ -5,6 +5,12 @@
  */
 package com.mycompany.exemplotrycatch;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+        
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author devsys-a
@@ -12,5 +18,36 @@ package com.mycompany.exemplotrycatch;
 public class ExmploTry {
     public static void main(String[] args) {
         
+        /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+
+
+        Scanner ler = new Scanner(System.in);
+        try {
+
+            int n = ler.nextInt();
+            System.out.println(30 / n);
+
+        } catch (InputMismatchException erro) {
+            System.out.print("voce so pode informar numeros inteiros!: " + erro);
+
+        } catch (ArithmeticException erro) {
+            System.out.println("Nnenhum numero pode ser dividido por 0" + erro);
+
+        } finally{
+        
+        System.out.println("FIM");
+        
+        }
+
     }
+
 }
+    
+
